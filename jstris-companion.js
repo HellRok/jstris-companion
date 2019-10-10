664bd82e1757ef4355459fba8dbd7a6c0f57ea75
Sean Earle <sean.r.earle@gmail.com>
2019-10-10 17:10:19 +1030

    Lots of changes since corruption of git repo

--- jstris-companion.js
+++ jstris-companion.js
@@ -20,14 +20,40 @@ class JstrisCompanion {
     `);
   }
 
-  setSecondCCW(keyCode) {
-    const duplicateCCW = (e) => {
-      if (e.keyCode !== keyCode) { return };
-      const event = new KeyboardEvent(e.type, {keyCode : 67});
+  setGhost(ghost, size) {
+    this.injectJavascript(`
+      loadGhostSkin('${ghost}', ${size});
+    `);
+  }
+
+  setSecondKey(newKey, oldKey) {
+    const newKeyCode = parseInt(newKey);
+    const oldKeyCode = this.settings()[oldKey];
+
+    const duplicateKey = (e) => {
+      if (e.keyCode !== newKeyCode) { return };
+      const event = new KeyboardEvent(e.type, {keyCode : oldKeyCode});
       document.dispatchEvent(event);
     }
-    window.addEventListener("keydown", duplicateCCW, true);
-    window.addEventListener("keyup", duplicateCCW, true);
+    window.addEventListener("keydown", duplicateKey, true);
+    window.addEventListener("keyup", duplicateKey, true);
+  }
+
+  setSettings() {
+    this.injectJavascript(`
+      const dataDiv = document.createElement('div');
+      // Set the defaults unless window.sts is defined.
+      const settings = window.sts || {
+        k0: 37, k1: 39, k2: 40, k3: 32, k4: 67, k5: 38, k7: 65, k6: 67
+      }
+      dataDiv.id = 'jstris_data';
+      dataDiv.dataset['settings'] = JSON.stringify(settings);
+      (document.body || document.head || document.documentElement).appendChild(dataDiv);
+    `);
+  }
+
+  settings() {
+    return JSON.parse(document.getElementById('jstris_data').dataset['settings']);
   }
 
   injectJavascript(code) {
@@ -39,9 +65,7 @@ class JstrisCompanion {
   injectStylesheet(code) {
     const style = document.createElement('style');
     style.type = 'text/css'
-    style.appendChild(document.createTextNode(`
-        ${code}
-    `));
+    style.appendChild(document.createTextNode(code));
     this.append(style);
   }
 
@@ -50,9 +74,58 @@ class JstrisCompanion {
   }
 }
 
-window.addEventListener('load', function(){
-  const jstrisCompanion = new JstrisCompanion();
-  jstrisCompanion.setBackground('https://i.imgur.com/hwL7ZsY.png');
-  jstrisCompanion.setSkin("https://i.imgur.com/8eIewoR.png", 32);
-  jstrisCompanion.setSecondCCW(221);
+const jstrisCompanion = new JstrisCompanion();
+window.addEventListener('load', () => { jstrisCompanion.setSettings(); });
+
+window.addEventListener('load', () => {
+  const keys = ['skin', 'skinPixels', 'ghost', 'ghostPixels', 'background',
+    'left', 'right', 'soft', 'hard', 'rotateLeft', 'rotateRight',
+    'rotate180', 'hold', 'customCss', 'customJs'];
+  const isSet = value => {
+    return value !== null && value !== '' && typeof value !== 'undefined';
+  };
+
+  chrome.storage.sync.get(keys, (result) => {
+    if (isSet(result.skin) && isSet(result.skinPixels)) {
+      jstrisCompanion.setSkin(result.skin, result.skinPixels);
+    }
+    if (isSet(result.ghost) && isSet(result.ghostPixels)) {
+      jstrisCompanion.setGhost(result.ghost, result.ghostPixels);
+    }
+    if (isSet(result.background)) {
+      jstrisCompanion.setBackground(result.background);
+    }
+
+    if (isSet(result.left)) {
+      jstrisCompanion.setSecondKey(result.left, 'k0');
+    }
+    if (isSet(result.right)) {
+      jstrisCompanion.setSecondKey(result.right, 'k1');
+    }
+    if (isSet(result.soft)) {
+      jstrisCompanion.setSecondKey(result.soft, 'k2');
+    }
+    if (isSet(result.hard)) {
+      jstrisCompanion.setSecondKey(result.hard, 'k3');
+    }
+    if (isSet(result.rotateLeft)) {
+      jstrisCompanion.setSecondKey(result.rotateLeft, 'k4');
+    }
+    if (isSet(result.rotateRight)) {
+      jstrisCompanion.setSecondKey(result.rotateRight, 'k5');
+    }
+    if (isSet(result.rotate180)) {
+      jstrisCompanion.setSecondKey(result.rotate180, 'k7');
+    }
+    if (isSet(result.hold)) {
+      jstrisCompanion.setSecondKey(result.hold, 'k6');
+    }
+
+    if (isSet(result.customCss)) {
+      jstrisCompanion.injectStylesheet(result.customCss);
+    }
+    if (isSet(result.customJs)) {
+      jstrisCompanion.injectJavascript(result.customJs);
+    }
+  });
 });
--- popup.js
+++ popup.js
@@ -1,3 +1,57 @@
-document.addEventListener("DOMContentLoaded",function(){
-  alert("HI");
+class JstrisCompanionPopup {
+  constructor() {
+    this.keys = ['skin', 'skinPixels', 'ghost', 'ghostPixels', 'background',
+      'left', 'right', 'soft', 'hard', 'rotateLeft', 'rotateRight',
+      'rotate180', 'hold', 'customCss', 'customJs'];
+  }
+
+  setup() {
+    this.defineAttributes();
+    this.populateForm();
+    this.setupListeners();
+  }
+
+  defineAttributes() {
+    const _this = this;
+    this.keys.forEach((key) => {
+      _this[key] = document.getElementById(key);
+    });
+  }
+
+  populateForm() {
+    const _this = this;
+    chrome.storage.sync.get(this.keys, (result) => {
+      console.log(result);
+      _this.keys.forEach((key) => {
+        console.log(key, _this[key], result[key]);
+        _this[key].value = result[key];
+      });
+    });
+  }
+
+  setupListeners() {
+    const _this = this;
+    document.getElementById('save').addEventListener('click', () => { _this.persistValues() });
+
+    document.querySelectorAll('.key').forEach((elem) => {
+      elem.addEventListener('keydown', (event) => {
+        event.target.value = event.keyCode;
+      });
+    });
+  }
+
+  persistValues() {
+    let opts = {}
+    this.keys.forEach((key) => {
+      opts[key] = this[key].value;
+    });
+
+    chrome.storage.sync.set(opts);
+    document.getElementById('restartInfo').classList.remove('hide');
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const popup = new JstrisCompanionPopup();
+  popup.setup();
 });
